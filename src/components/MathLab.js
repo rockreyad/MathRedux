b807ad97a9c72279243f3ee588796f1bbe5ca827
Rockreyad <rockreyad@gmail.com>
2022-05-21 13:36:26 +0600

    button handler added

--- src/components/MathLab.js
+++ src/components/MathLab.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/index";
 
 const MathLab = () => {
+  const dispatch = useDispatch();
+  const { addNumber, subNumber, mulNumber, divNumber } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
   return (
     <div>
       <div className="bg-gray-600 rounded-xl w-72">
@@ -9,25 +17,37 @@ const MathLab = () => {
           <div className="flex justify-around bg-blue-800 bg-opacity-20 backdrop-filter rounded-b-xl">
             <div className="text-white  p-2">
               <p className="">Add</p>
-              <button className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50">
+              <button
+                className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50"
+                onClick={() => addNumber(1)}
+              >
                 +
               </button>
             </div>
             <div className="text-white  p-2">
               <p className="">Sub</p>
-              <button className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50">
+              <button
+                className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50"
+                onClick={() => subNumber(1)}
+              >
                 -
               </button>
             </div>
             <div className="text-white  p-2">
               <p className="">Mul</p>
-              <button className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50">
+              <button
+                className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50"
+                onClick={() => mulNumber(2)}
+              >
                 *
               </button>
             </div>
             <div className="text-white  p-2">
               <p className="">Div</p>
-              <button className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50">
+              <button
+                className="w-7 py-1 px-2 bg-gray-400 rounded-md hover:bg-gray-200 hover:text-black font-semibold my-1 shadow-md shadow-slate-400 hover:shadow-slate-50"
+                onClick={() => divNumber(2)}
+              >
                 /
               </button>
             </div>
